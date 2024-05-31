bd5a5750ab8da85c485e5365751ad28aee8fd0a0
KIMSMING <alstjdrla78@gmail.com>
2024-05-31 20:32:31 +0900

    add study001
    add study001

---
+++ Study001/src/test/java/com/studymavenspringboot/study001/exam001Test.java
@@ -0,0 +1,34 @@
+package com.studymavenspringboot.study001;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class exam001Test {
+    @Test
+    public void subtract001() throws Exception{
+        //given     테스트하기 위한 준비 동작
+        // int num1 = -50001;
+        // int num2 = 3;
+        // int expected = -50004;
+
+        //when      테스트 실시
+        // int actual = subTest001(num1, num2);
+
+        //then      실체값과 기대값을 비교해서 테스트 한다
+        assertThat(-1).isEqualTo(subTest001(2,3));
+        assertThat(98).isEqualTo(subTest001(100, 2));
+        assertThat(3).isEqualTo(subTest001(10, 7));
+        assertThat(10).isLessThan(subTest001(100, 70));
+        assertThat(32).isGreaterThan(subTest001(100, 70));
+        //      assertThat(actual).isEqualTo(expected).describedAs("실제값과 다름");
+    }
+    int subTest001(int n1, int n2) throws Exception{
+        if( n1 < -50000 || n1 > 50000){
+            throw new Exception("num1 값은 -50001보다 작거나 num1 값은 50000보다 큽니다");
+        }
+        if( n2 < -50000 || n2 > 50000){
+            throw new Exception("num2 값은 -50001보다 작거나 num2 값은 50000보다 큽니다");
+        }
+        return n1 - n2;
+    }
+}
