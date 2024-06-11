9a7399c909bbcc20b662d26aebe2540f152970f0
KIMSMING <alstjdrla78@gmail.com>
2024-06-11 22:11:36 +0900

    Update Student.java

--- Study001/src/main/java/com/studymavenspringboot/study001/mathExam/Student.java
+++ Study001/src/main/java/com/studymavenspringboot/study001/mathExam/Student.java
@@ -1,17 +1,29 @@
 package com.studymavenspringboot.study001.mathExam;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class Student {
-    private String name;    //멤버편수(필드)는 무조건 private, Getter, Setter을 만들어야 한다.
+    private String name;
     private int score;
 
-    @Override
+    public Student() {
+    }
+
     public String toString() {
-        return String.format("이름 : %s, 점수 : %d, %s"
-                , name, score, super.toString());
+        return String.format("이름 : %s, 점수 : %d, %s", this.name, this.score, super.toString());
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public void setScore(final int score) {
+        this.score = score;
     }
 }
