0614f48aaa3300cd06f7763c31b3690ef7c34b6f
KIMSMING <alstjdrla78@gmail.com>
2024-06-13 21:30:23 +0900

    계좌를 관리하는 프로그램 (분석 필요)
    계좌를 관리하는 프로그램 (분석 필요)

--- Study001/src/main/java/com/studymavenspringboot/study001/mathExam/Account.java
+++ Study001/src/main/java/com/studymavenspringboot/study001/mathExam/Account.java
@@ -4,9 +4,16 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class Account {
+    @Setter
+    @Getter
     private String num;
+    @Setter
+    @Getter
     private String name;
+    @Setter
+    @Getter
     private int money;
+    private int hap;
 
     public Account(String num, String name, int money) {
         this.num = num;
@@ -14,26 +21,7 @@ public class Account {
         this.money = money;
     }
 
-
-    public String getNum(){
-            return num;
-        }
-        public void setNum(String num){
-            this.num = num;
-        }
-        public String getName(){
-            return name;
-        }
-        public void setName(String name){
-            this.name = name;
-        }
-        public int getMoney(){
-            return money;
-        }
-        public void setMoney(int money){
-            this.money = money;
-        }
-     /*
+    /*
     static final int MIN_BALANCE = 0;
     static final int MAX_BALANCE = 1000000;
     private int balance;
--- Study001/src/main/java/com/studymavenspringboot/study001/mathExam/BankApplication.java
+++ Study001/src/main/java/com/studymavenspringboot/study001/mathExam/BankApplication.java
@@ -1,16 +1,14 @@
 package com.studymavenspringboot.study001.mathExam;
 import java.util.Scanner;
-import java.util.Arrays;
-
 public class BankApplication {
     static Scanner sc = new Scanner(System.in);
-    static Account[] AccountArray = new Account[100];
+    static Account[] accountArray = new Account[100];
+    static int t = 0, x = 0;
     public static void main(String[] args){
-
         boolean run = true;
         while(run){
             System.out.println("------------------------------");
-            System.out.println("1.계좌생성|2.계좌목록|3.예금|4.출금|5.종료");
+            System.out.println("1.계좌생성 | 2.계좌목록 | 3.예금 | 4.출금 | 5.종료");
             System.out.print("선택 >> ");
             int i = sc.nextInt();
             if( i == 1 ){
@@ -28,7 +26,6 @@ public class BankApplication {
         }
         System.out.println("프로그램 종료");
     }
-    static int i = 0;
     private static void createAccount(){
         System.out.println("------------------------------");
         System.out.println("계좌생성");
@@ -40,26 +37,64 @@ public class BankApplication {
         System.out.print("초기입금액 : ");
         int money = sc.nextInt();
         System.out.println("계좌가 생성되었습니다");
-/*
-        AccountArray[i] = new Account;
+        Account newAccount = new Account(num, name, money);
+        accountArray[t] = newAccount;
+        t++;
 
-        i++;
-
-
- */
     }
     private static void accountList(){
         System.out.println("------------------------------");
         System.out.println("계좌목록");
         System.out.println("------------------------------");
-        for(int i = 0; i < AccountArray.length;i++){
-            System.out.println(Arrays.toString(new Account[]{AccountArray[i]}));
+        for(int j = 0; j < t;j++){
+            Account account = accountArray[j];
+            if( account != null){
+                System.out.println(account.getNum() + "   " + account.getName() + "   " + account.getMoney());
+            }
         }
     }
     private static void deposit(){
-
+        System.out.println("------------------------------");
+        System.out.println("예금");
+        System.out.println("------------------------------");
+        System.out.print("계좌번호: ");
+        String num = sc.next();
+        System.out.print("예금액: ");
+        int money = sc.nextInt();
+        for(int j = 0; j < t; j++){
+            Account account = accountArray[j];
+            if(account.getNum().equals(num)){
+                x = account.getMoney();
+                x += money;
+                accountArray[j].setMoney(x);
+                System.out.println(accountArray[j].getMoney());
+            }
+        }
     }
+
     private static void withdraw(){
+        System.out.println("------------------------------");
+        System.out.println("출금");
+        System.out.println("------------------------------");
+        System.out.print("계좌번호: ");
+        String num = sc.next();
+        System.out.print("출금액: ");
+        int money = sc.nextInt();
+        for(int j = 0; j < t; j++){
+            Account account = accountArray[j];
+            if(account.getNum().equals(num)){
+                if(account.getMoney() < money) {
+                    System.out.println("결과 : 출금이 실패했습니다");
+                    return;
+                }else{
+                    x = account.getMoney();
+                    x -= money;
+                    accountArray[j].setMoney(x);
+                    System.out.println("결과 : 출금이 성공했습니다");
+                    System.out.println(accountArray[j].getMoney());
+                }
+            }
+        }
 
     }
 }
