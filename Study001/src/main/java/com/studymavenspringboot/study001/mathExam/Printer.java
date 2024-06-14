d3764208ab48cd2674129acea232d6bab44e3cd8
KIMSMING <alstjdrla78@gmail.com>
2024-06-14 18:09:53 +0900

    19번까지
    19번까지

--- Study001/src/main/java/com/studymavenspringboot/study001/mathExam/Printer.java
+++ Study001/src/main/java/com/studymavenspringboot/study001/mathExam/Printer.java
@@ -1,19 +1,22 @@
 package com.studymavenspringboot.study001.mathExam;
 
-class Printer {
-    public static void println(int value) {
-        System.out.println(value);
+public class Printer {
+    public static void println(int x) {
+        System.out.println(x);
     }
-
-    public static void println(boolean value) {
-        System.out.println(value);
+    public static void println(boolean x) {
+        System.out.println(x);
     }
-
-    public static void println(double value) {
-        System.out.println(value);
+    public static void println(double x) {
+        System.out.println(x);
     }
-
-    public static void println(String value) {
-        System.out.println(value);
+    public static void println(String x) {
+        System.out.println(x);
+    }
+    public static void main(String[] args) {
+        Printer.println(10);
+        Printer.println(true);
+        Printer.println(5.7);
+        Printer.println("홍길동");
     }
 }
--- Study001/src/main/java/com/studymavenspringboot/study001/mathExam/ShopService.java
+++ Study001/src/main/java/com/studymavenspringboot/study001/mathExam/ShopService.java
@@ -1,11 +1,23 @@
 package com.studymavenspringboot.study001.mathExam;
 
 public class ShopService {
-    private static ShopService singleton = new ShopService();
+    private static ShopService shopservice = new ShopService();
 
-    private ShopService(){}
-
-    public static ShopService getInstance(){
-        return singleton;
+    private ShopService() {
     }
+
+    public static ShopService getInstance() {
+        return shopservice;
+    }
+    public static void main(String[] args) {
+        ShopService obj1 = ShopService.getInstance();
+        ShopService obj2 = ShopService.getInstance();
+
+        if (obj1 == obj2) {
+            System.out.println("같은 ShopService 객체입니다.");
+        } else {
+            System.out.println("다른 ShopService 객체입니다.");
+        }
+    }
+
 }
