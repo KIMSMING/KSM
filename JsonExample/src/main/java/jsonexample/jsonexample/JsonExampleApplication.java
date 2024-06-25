2ad15a269bce7596bfc0ce49a0fe561282d03b87
KIMSMING <alstjdrla78@gmail.com>
2024-06-25 15:25:08 +0900

    인터페이스 활용

--- JsonExample/src/main/java/jsonexample/jsonexample/JsonExampleApplication.java
+++ JsonExample/src/main/java/jsonexample/jsonexample/JsonExampleApplication.java
@@ -1,13 +1,13 @@
-package jsonexample.jsonexample;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class JsonExampleApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(JsonExampleApplication.class, args);
-    }
-
-}
+//package jsonexample.jsonexample;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class JsonExampleApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(JsonExampleApplication.class, args);
+//    }
+//
+//}
--- JsonExample/src/test/java/jsonexample/jsonexample/JsonExampleApplicationTests.java
+++ JsonExample/src/test/java/jsonexample/jsonexample/JsonExampleApplicationTests.java
@@ -1,13 +1,13 @@
-package jsonexample.jsonexample;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class JsonExampleApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package jsonexample.jsonexample;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JsonExampleApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
