f8c49a357e6de91200133d9d418bf54e3df20c98
KIMSMING <alstjdrla78@gmail.com>
2024-06-25 17:31:13 +0900

    시험
    시험

---
+++ TestFile0628/src/main/java/id/testfile0628/TestFile0628Application.java
@@ -0,0 +1,13 @@
+package id.testfile0628;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFile0628Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestFile0628Application.class, args);
+    }
+
+}
---
+++ TestFile0628/src/test/java/id/testfile0628/TestFile0628ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.testfile0628;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFile0628ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
